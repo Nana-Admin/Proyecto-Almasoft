f8f2a8c57330f52ac687f631ceb75dce384b7ebb
Nana-Admin <dannykarineromero@gmail.com>
2025-07-10 15:19:46 -0500

    vistas admin

--- admin/vistas/index/javascript.js
+++ admin/vistas/index/javascript.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.getElementById('login-form').addEventListener('submit', function(e) {
     e.preventDefault();
 
@@ -11,3 +12,18 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
     }
 });
 
+=======
+document.getElementById('login-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value;
+
+    if (username === 'admin' && password === '1234') {
+        window.location.href = '../inicio/inicio.html';
+    } else {
+        alert('Usuario o contraseña incorrectos');
+    }
+});
+
+>>>>>>> 856621e9eb9b117d84efbbe499c18a2454807570
--- admin/vistas/tramite/tramites.js
+++ admin/vistas/tramite/tramites.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.getElementById("buscador").addEventListener("input", function () {
   const filtro = this.value.toLowerCase();
   const filas = document.querySelectorAll("#tabla tbody tr");
@@ -7,3 +8,14 @@ document.getElementById("buscador").addEventListener("input", function () {
     fila.style.display = texto.includes(filtro) ? "" : "none";
   });
 });
+=======
+document.getElementById("buscador").addEventListener("input", function () {
+  const filtro = this.value.toLowerCase();
+  const filas = document.querySelectorAll("#tabla tbody tr");
+
+  filas.forEach(fila => {
+    const texto = fila.textContent.toLowerCase();
+    fila.style.display = texto.includes(filtro) ? "" : "none";
+  });
+});
+>>>>>>> 856621e9eb9b117d84efbbe499c18a2454807570
